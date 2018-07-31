d8d023d163a8a8d0dfeb740b15bcab6d19cbc2ce
willc <lerebel103@gmail.com>
2018-07-31 22:34:30 +0800

    Added json config. Separated out controller class.

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import logging
 import sys
 import time
@@ -6,51 +7,64 @@ import time
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt_client
 
-from config import AgentConfig
+from controller import TempController
 from pacer import Pacer
 from peripherals.blower_fan import BlowerFan
 from peripherals.temperature_sensors import Max31850Sensors
-from pid import PID
 
 logger = logging.getLogger(__name__)
 
 
 class Agent:
     def __init__(self):
-        # Init GPIO
-        GPIO.setmode(GPIO.BCM)
-
-        self._config = AgentConfig()
-        self._go = False
-        self._temp_sensors = Max31850Sensors(self._config.temperature_gpio, self._config.temperature_sample_interval)
-        self._blower_fan = BlowerFan(self._config.blower_fan_gpio_relay,
-                                     self._config.blower_fan_gpio_pwm,
-                                     self._config.blower_fan_gpio_rpm)
-
-        # Our MQTT client
-        self._client = mqtt_client.Client()
+        # Load configuration
+        with open('config.json') as f:
+            config = json.load(f)
+        self._config = config
 
         # Setup logger
         root = logging.getLogger()
-        root.setLevel(self._config.logger.level)
+        level = self._config['logger']['level']
+        root.setLevel(level)
 
         ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(self._config.logger.level)
+        ch.setLevel(level)
         formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
         ch.setFormatter(formatter)
         root.addHandler(ch)
 
+        # GPIOs
+        GPIO.setmode(GPIO.BCM)
+
+        self._go = False
+        self._temp_sensors = Max31850Sensors(self._config['gpios']['temperature'],
+                                             self._config['intervals']['temperature_second'])
+        self._blower_fan = BlowerFan(self._config['gpios']['blower_fan_relay'],
+                                     self._config['gpios']['blower_fan_pwm'],
+                                     self._config['gpios']['blower_fan_rpm'])
+
+        # Our MQTT client
+        self._client = mqtt_client.Client()
+
+        # IController
+        self._controller = TempController(self._config, self._temp_sensors, self._blower_fan, self._client)
+
     def initialise(self):
         logger.info('Initialising.')
 
-        # Initialise peripherals
-        self._temp_sensors.initialise()
-        self._blower_fan.initisalise()
-
         # Connect to MQTT
-        self._client.connect_async(host=self._config.mqtt.host, port=self._config.mqtt.port)
+        self._client.connect_async(host=self._config["mqtt"]["broker_host"],
+                                   port=self._config["mqtt"]["broker_port"])
+
         # Start control loop
+        self._client.on_message = self._on_mqtt_message
+
+        # subscribe to our control topics
+        self._client.subscribe(self._config['mqtt']['root_topic'] + 'controller')
+
         self._client.loop_start()
+        # Initialise controller
+        self._controller.initialise()
 
     def run(self):
         self._go = True
@@ -61,10 +75,6 @@ class Agent:
         # Causes the control loop to stop
         self._go = False
 
-        # Turn off fan as precaution
-        if self._blower_fan.is_on:
-            self._blower_fan.off()
-
         # Terminates communications.
         self._client.loop_stop()
 
@@ -74,54 +84,23 @@ class Agent:
     def _control_loop(self):
         logger.info('Running control loop.')
         pacer = Pacer()
-        self._blower_fan.on()
-        self._temp_sensors.on()
-
-        # Here's our PID
-        pid = PID()
-        pid.set_point = 40
-
+        self._controller.start()
         try:
             while self._go:
                 now = time.time()
+                self._controller.tick(now)
 
-                # Read sensor temps
-                bbq_temp = None
-                temps = ''
-                for sensor in self._temp_sensors.sensors:
-                    temp, status = self._temp_sensors.sensor_temp(sensor)
-                    if temp is not None and status == Max31850Sensors.Status.OK:
-                        bbq_temp = temp
-                        temps += '[{:.3f}, {}] '.format(temp, status)
-                    else:
-                        bbq_temp = None
-                        temps += '[--, {}] '.format(status)
-
-                if bbq_temp is not None:
-                    output = pid.update(now, bbq_temp)
-                    # Set fan duty cycle
-                    print(output)
-                    self._blower_fan.duty_cycle = output
-
-                rpm = self._blower_fan.rpm
-
-                if not self._blower_fan.is_healthy:
-                    print("**************** FAN NOT SPINNING.")
-
-                # Tick all parts of the system from here
-                msg = 'rpm={} temps={}, board={}'.format(rpm, temps, self._temp_sensors.board_temp())
-                self._client.publish('test', msg, qos=1)
-
-                logger.info(msg)
                 # Pace control loop per desired interval
                 try:
-                    pacer.pace(now, self._config.control_loop_seconds)
+                    pacer.pace(now, self._config['intervals']['control_loop_second'])
                 except KeyboardInterrupt:
                     self.terminate()
         finally:
-            self._temp_sensors.off()
-            self._blower_fan.off()
+            self._controller.stop()
             logger.info('Control loop terminated.')
+
+    def _on_mqtt_message(self, client, userdata, message):
+        pass
 
 
 if __name__ == "__main__":
---
+++ controller.py
@@ -0,0 +1,62 @@
+import logging
+
+from peripherals.temperature_sensors import Max31850Sensors
+from pid import PID
+
+logger = logging.getLogger(__name__)
+
+
+class TempController:
+
+    def __init__(self, config, sensors, blower_fan, client):
+        self._config = config
+        self._client = client
+        self._pid = PID()
+        self._temp_sensors = sensors
+        self._blower_fan = blower_fan
+
+    def initialise(self):
+        # Initialise peripherals
+        self._temp_sensors.initialise()
+        self._blower_fan.initisalise()
+
+
+    def start(self):
+        logger.info('Controller starting.')
+        self._blower_fan.on()
+        self._temp_sensors.on()
+
+    def stop(self):
+        self._temp_sensors.off()
+        self._blower_fan.off()
+        logger.info('Controller stopped.')
+
+    def tick(self, now):
+        # Read sensor temps
+        bbq_temp = None
+        temps = ''
+        for sensor in self._temp_sensors.sensors:
+            temp, status = self._temp_sensors.sensor_temp(sensor)
+            if temp is not None and status == Max31850Sensors.Status.OK:
+                bbq_temp = temp
+                temps += '[{:.3f}, {}] '.format(temp, status)
+            else:
+                bbq_temp = None
+                temps += '[--, {}] '.format(status)
+
+        if bbq_temp is not None:
+            output = self._pid.update(now, bbq_temp)
+            # Set fan duty cycle
+            print(output)
+            self._blower_fan.duty_cycle = output
+
+        rpm = self._blower_fan.rpm
+
+        if not self._blower_fan.is_healthy:
+            print("**************** FAN NOT SPINNING.")
+
+        # Tick all parts of the system from here
+        msg = 'rpm={} temps={}, board={}'.format(rpm, temps, self._temp_sensors.board_temp())
+        self._client.publish('test', msg, qos=1)
+
+        logger.info(msg)

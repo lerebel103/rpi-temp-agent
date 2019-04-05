8a7159733cb49a4cf37b59aba43781e8888c95cb
willc <lerebel103@gmail.com>
2019-04-05 21:34:23 +0800

    Added accumulator to get derivative.

---
+++ accumulator.py
@@ -0,0 +1,48 @@
+import numpy as np
+import bisect
+
+
+class Accumulator:
+
+    def __init__(self, interval_max):
+        self.interval_max = interval_max
+        self._x = []
+        self._y = []
+
+    def add(self, timestamp, value):
+        self._x.append(timestamp)
+        self._y.append(value)
+        self._trim()
+
+    def length(self):
+        return len(self._x)
+
+    def interval(self):
+        """ Accumulated interval up to this point. """
+        if self.length() > 0:
+            return self._x[-1] - self._x[0]
+        else:
+            return 0
+
+    def reset(self):
+        """ Flush everything to start over again. """
+        self._x = []
+        self._y = []
+
+    def linear_derivative(self, from_time=None):
+        """ Linear fit derivative Celcius/second, delta second from desired point """
+        if self.length() < 1:
+            return 0
+        elif from_time is None:
+            return np.polyfit(self._x, self._y, 1)[0]
+        else:
+            idx = bisect.bisect(self._x, from_time)
+            if 0 <= idx < self.length():
+                return np.polyfit(self._x[idx:], self._y[idx:], 1)[0]
+            else:
+                return 0
+
+    def _trim(self):
+        while self.length() > 0 and self.interval() > self.interval_max:
+            self._x.pop(0)
+            self._y.pop(0)
---
+++ tests/unit/test_accumulator.py
@@ -0,0 +1,75 @@
+import unittest
+import time
+
+from accumulator import Accumulator
+
+
+class TestAccumulator(unittest.TestCase):
+
+    def test_max_interval(self):
+        """ Make sure internal queue keeps just as many items as we need """
+        acc = Accumulator(50)
+
+        for k in range(10):
+            t = time.time()
+
+            for delta in range(100):
+                acc.add(t + delta, delta)
+
+            self.assertEqual(51, acc.length())
+            self.assertEqual(50, acc.interval())
+
+            # go over again and we get the same result
+            acc.reset()
+    
+    def test_derivative_all(self):
+        """ Make sure derivative is what we expect on sample set """
+        acc = Accumulator(50)
+
+        t = time.time()
+        for delta in range(100):
+            acc.add(t + delta, delta)
+
+        # Then we have 1 here
+        self.assertAlmostEqual(1, acc.linear_derivative(), 1)
+
+        acc.reset()
+
+        # reverse
+        for delta in range(100):
+            acc.add(t + delta, 100 - delta)
+
+        # Then we have 1 here
+        self.assertAlmostEqual(-1, acc.linear_derivative(), 1)
+
+    def test_derivative_delta_back(self):
+        """ get derivative from a specific point back """
+        acc = Accumulator(100)
+
+        # zero points
+        self.assertAlmostEqual(0, acc.linear_derivative(), 1)
+
+        t = time.time()
+        for delta in range(50):
+            acc.add(t + delta, delta)
+
+        # Then we have 1 here
+        self.assertAlmostEqual(1, acc.linear_derivative(), 1)
+
+        # reverse
+        for delta in range(50, 100):
+            acc.add(t + delta, 100 - delta)
+
+        # So we have zero here for the lot
+        self.assertAlmostEqual(0, acc.linear_derivative(), 1)
+
+        # exactly mid point, we have -1 by definition
+
+        # Then we have 1 here
+        self.assertAlmostEqual(-1, acc.linear_derivative(t+50), 1)
+        # same forward
+        self.assertAlmostEqual(-1, acc.linear_derivative(t+80), 1)
+
+        # on edge?
+        self.assertAlmostEqual(0, acc.linear_derivative(t+99), 1)
+        self.assertAlmostEqual(0, acc.linear_derivative(t+100), 1)

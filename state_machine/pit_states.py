379ed822554a213c3714989728a96642c92cc1f7
willc <lerebel103@gmail.com>
2019-04-06 11:34:38 +0800

    Added accumulator to get derivative.

--- state_machine/pit_states.py
+++ state_machine/pit_states.py
@@ -8,13 +8,18 @@ logger = logging.getLogger(__name__)
 class PitInitial(BaseSensorState):
     def handle_temp(self, temp, set_point):
         if temp < set_point:
-            return self
+            return ComingToTemp()
         else:
-            return self
+            return OverTemp()
 
 
 class ComingToTemp(BaseSensorState):
     def handle_temp(self, temp, set_point):
+
+        last_minute = self.ctx.timestamp - 60
+        print('** derivative ' +
+              self.ctx.accumulators[self.sensor_name].linear_derivative(last_minute))
+
         return self
 
 
